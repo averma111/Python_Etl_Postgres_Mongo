af3c0c6814337d39645c56b125af81ab69a81cd3
averma111 <ashishn8751@outlook.com>
2021-10-03 14:41:26 -0700

    Updated the main and performetl to have hire_date

--- etl/performetl.py
+++ etl/performetl.py
@@ -1,5 +1,6 @@
 # Importing the dependencies
 import copy
+from datetime import datetime
 
 # Pipeline constants
 RESET_MONGO_COLLECTIONS_ON_UPDATE = True
@@ -9,7 +10,7 @@ class Performetl:
 
     def extarct_postgres_data(self, pg_cursor):
         pg_cursor.execute(
-            """select id,first_name,last_name from employees.employee""")
+            """select id,first_name,last_name,hire_date from employees.employee""")
         table_employee = pg_cursor.fetchall()
         return table_employee
 
@@ -22,6 +23,7 @@ class Performetl:
                 temp_collection['id'] = item[0]
                 temp_collection['first_name'] = item[1]
                 temp_collection['last_name'] = item[2]
+                temp_collection['hire_date'] = str(item[3])
                 dataset_collection.append(copy.copy(temp_collection))
 
             return dataset_collection
